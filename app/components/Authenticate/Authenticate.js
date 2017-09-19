45c261a899b9c7b6f8571fb1885ed2abd79f38ea
Juan Portillo <juan@goowee.io>
2017-09-18 21:54:19 -0600

    fix auth

--- app/components/Authenticate/Authenticate.js
+++ app/components/Authenticate/Authenticate.js
@@ -1,7 +1,19 @@
 import React, { PropTypes } from 'react'
+import { centeredContainer, largeHeader, errorMsg } from 'sharedStyles/styles.css'
+import {FacebookAuthButton} from 'components'
 
-export default function Authenticate (props) {
+Authenticate.PropTypes= {
+  error: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  onAuth: PropTypes.func.isRequire,
+}
+
+export default function Authenticate ({ error, isFetching, onAuth}) {
   return (
-    <div>Authenticate</div>
+    <div className={centeredContainer}>
+      <h1 className={largeHeader}>{'Authenticate'}</h1>
+      <FacebookAuthButton isFetching={isFetching} onAuth={onAuth} />
+      {error ? <p className={errorMsg}>{error}</p> : null}
+    </div>
   )
 }
